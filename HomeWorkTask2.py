7775f87a8e73168d369802b014640193df128f6f
posthak <adler_m@mail.ru>
2022-12-13 23:54:32 +0600

    HW2

--- HomeWorkTask2.py
+++ HomeWorkTask2.py
@@ -7,7 +7,7 @@
 print('Input 3 digits number: ')
 a = int(input())
 if len(str(a)) == 3:
-    res = a%10 + int(a/10)%10 + int(a/100)%10
+    res = a % 10 + int(a/10) % 10 + int(a/100) % 10
     print(f"{a} -> {res} ({int(a/100)%10} + {int(a/10)%10} + {a%10})")
 else:
     print('You have entered wrong number')
